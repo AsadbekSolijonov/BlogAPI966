04ad815ed42ef2127bbb293846a7038cab0e2ab0
SolijonovAsadbek <solijonovasadbek25111999@gmail.com>
2024-12-08 10:28:50 +0500

    first commit

---
+++ blog/serializers.py
@@ -0,0 +1,39 @@
+from rest_framework import serializers
+from blog.models import Blog
+
+
+class BlogListSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Blog
+        fields = ['id', 'title', 'description']
+
+
+class BlogDetailSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Blog
+        fields = '__all__'
+
+
+class BlogSearchSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Blog
+        exclude = ['id']
+
+
+class BlogCreateSerizlier(serializers.ModelSerializer):
+    class Meta:
+        model = Blog
+        fields = '__all__'
+
+
+class BlogUpdateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Blog
+        fields = '__all__'
+
+    def update(self, instance, validated_data):
+        print(validated_data.get('title', instance.title))
+        instance.title = validated_data.get('title', instance.title)
+        instance.description = validated_data.get('description', instance.description)
+        instance.save()
+        return instance
